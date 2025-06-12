d9746c12db711c8094225c728b6011dc695e8be4
yangsong13 <yangsong13@meituan.com>
2025-06-12 19:50:00 +0800

    first commit

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -17,8 +17,8 @@ const getBasePath = () => {
 
 const data: ISiteMetadataResult = {
   siteTitle: 'Workouts Map',
-  siteUrl: 'https://ben29.xyz',
-  logo: 'https://avatars3.githubusercontent.com/u/6956444?s=460&u=97e1062227c4088db3c987146455245c6fa78441',
+  siteUrl: 'https://workouts.owenyang.top',
+  logo: 'https://static.owenyang.top/blogs/avatar.jpg',
   description: 'Personal site and blog',
   keywords: 'workouts, running, cycling, riding, roadtrip, hiking, swimming',
   navLinks: [
@@ -28,11 +28,11 @@ const data: ISiteMetadataResult = {
     },
     {
       name: 'Blog',
-      url: 'https://ben29.xyz',
+      url: 'https://blog.owenyang.top',
     },
     {
       name: 'About',
-      url: 'https://github.com/ben-29/workouts_page/blob/master/README-CN.md',
+      url: 'https://owenyang.top/about',
     },
   ],
 };
